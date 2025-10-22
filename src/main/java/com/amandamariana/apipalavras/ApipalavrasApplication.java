26257d49b4b23df6be8d0441c5b4755ee7c2d777
mnnt <mariana.santos@gft.com>
2025-10-22 16:58:56 +0200

    :sparkles: Projeto Base

---
+++ src/main/java/com/amandamariana/apipalavras/ApipalavrasApplication.java
@@ -0,0 +1,13 @@
+package com.amandamariana.apipalavras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipalavrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipalavrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amandamariana/apipalavras/ApipalavrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amandamariana.apipalavras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipalavrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
