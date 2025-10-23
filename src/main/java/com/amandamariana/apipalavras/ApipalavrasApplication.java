54104f693ba6ff1909c309652835fb3ad3493160
mnnt <mnnt@gft.com>
2025-10-23 15:32:00 -0300

    teste Integrando com intellij

--- src/main/java/com/amandamariana/apipalavras/ApipalavrasApplication.java
+++ src/main/java/com/amandamariana/apipalavras/ApipalavrasApplication.java
@@ -2,7 +2,7 @@ package com.amandamariana.apipalavras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//teste1
 @SpringBootApplication
 public class ApipalavrasApplication {
 
