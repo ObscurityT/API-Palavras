52b6f240e909912cd043a93adc0024ca38b0f341
mnnt <mnnt@gft.com>
2025-10-23 15:34:59 -0300

    teste 2 commitando pelo git bash

--- src/main/java/com/amandamariana/apipalavras/ApipalavrasApplication.java
+++ src/main/java/com/amandamariana/apipalavras/ApipalavrasApplication.java
@@ -3,6 +3,7 @@ package com.amandamariana.apipalavras;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //teste1
+//teste2
 @SpringBootApplication
 public class ApipalavrasApplication {
 
