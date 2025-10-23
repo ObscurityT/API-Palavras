bf2ad42a9c090108f5b43b7a03212e69fc2147f7
mnnt <mnnt@gft.com>
2025-10-23 15:36:44 -0300

    ultimo teste

--- src/main/java/com/amandamariana/apipalavras/ApipalavrasApplication.java
+++ src/main/java/com/amandamariana/apipalavras/ApipalavrasApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //teste1
 //teste2
+//teste3
 @SpringBootApplication
 public class ApipalavrasApplication {
 
